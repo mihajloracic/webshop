0f62bf23058031b92ebef4bc681becaf0a9bc1eb
mihajloracic <mihajlo.rac@gmail.com>
2016-07-26 15:22:50 +0200

    Add files via upload
    Product page in progress, need pagination and some bug fixes

--- js/products.js
+++ js/products.js
@@ -1,3 +1,9 @@
+function addToCart(name, quantity,price){
+  cart.push({name,quantity,price});
+}
+
+var cart = [];
+var table = '';
 $(document).ready(function() {
     var max = 0;
     $("article.container").each(function(){
@@ -6,8 +12,60 @@ $(document).ready(function() {
       }
     });
 
-    $("article.container").each(function(){
-      $(this).height(max + max * 0.1);
+    $(".row.empty").each(function(){
+      $(this).height(max - $(this).parent().parent().height());
     });
-    console.log($("span.glyphicon").css('position'));
+    //var d = $("input.supply").data('stock');
+    var d;
+    $(".plus").click(function(event){
+      event.preventDefault();
+        d = $(this).parent().parent().parent().children('.row').children('input')[0];
+        var max = $(d).data('stock');
+        var current = $(d).val();
+        if(max <= current){
+
+        }else{
+          $(d).val(+current + 1);
+        }
+    });
+    $(".minus").click(function(event){
+      event.preventDefault();
+        d = $(this).parent().parent().parent().children('.row').children('input')[0];
+        var max = $(d).data('stock');
+        var current = $(d).val();
+        if(current <= 0 ){
+
+        }else{
+          $(d).val(+current - 1);
+        }
+    });
+    $(".supply").on('input',function(){
+      if($(this).val() > $(this).data('stock')){
+        $(this).val($(this).data('stock'));
+      }
+      if($(this).val() < 0){
+        $(this).val(0);
+      }
+    });
+    $(".addCart").click(function(event){
+      event.preventDefault();
+      var productName = $(this).parent().parent().siblings('h3').text();
+      d = $(this).parent().parent().parent().children('.row').children('input')[0];
+      var productQuantity = $(d).val();
+      var max = $(d).data('stock');
+      var price = $(this).parent().siblings().children()[0];
+      price = $(price).children()[0];
+      price = $(price).text();
+
+      if(!productQuantity == 0 && max >= productQuantity){
+          addToCart(productName,productQuantity,price);
+          $(d).data('stock',max-productQuantity);
+          $(cart).each(function(){
+            table = '<tr><td>' + this.name + '</td><td>' +  this.quantity + '</td><td>' + this.price +'</td><td>' + this.price * this.quantity +  '</td></tr>';
+            console.log(this);
+          });
+          $(".checkout").append(table);
+      }
+
+    });
 });
